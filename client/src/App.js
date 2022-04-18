8b28a40bf3126ba6cb954644d96f139ed02a0a40
Davi Banfi <davibanfi1579@outlook.com>
2022-04-18 10:35:42 -0300

    feat: add contracts functionalities to React

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,32 @@
 import React, { Component } from "react";
 import ItemManagerContract from "./contracts/ItemManager.json";
-import ItemContract from ".contracts/Item.json"
+import ItemContract from "./contracts/Item.json"
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { loaded: false };
+  state = { loaded: false, cost: 0, itemName: "Item 1" };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       this.web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
       this.accounts = await this.web3.eth.getAccounts();
 
-      // Get the contract instance.
       this.networkId = await this.web3.eth.net.getId();
-      this.itemManager = new web3.eth.Contract(
+      this.itemManager = new this.web3.eth.Contract(
         ItemManagerContract.abi,
-        ItemManagerContract.networks[networkId] && ItemManagerContract.networks[networkId].address,
+        ItemManagerContract.networks[this.networkId] && ItemManagerContract.networks[this.networkId].address,
       );
 
-      this.item = new web3.eth.Contract(
+      this.item = new this.web3.eth.Contract(
         ItemContract.abi,
-        ItemContract.networks[networkId] && ItemContract.networks[networkId].address,
+        ItemContract.networks[this.networkId] && ItemContract.networks[this.networkId].address,
       );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
 
       this.setState({ loaded: true });
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -55,6 +47,24 @@ class App extends Component {
     this.setState({ storageValue: response });
   };
 
+  handleInputChange = (event) => {
+    const target = event.target;
+
+    const value = target.type === "checkbox" ? target.checked : target.value;
+
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = async () => {
+    const { cost, itemName } = this.state;
+
+    await this.itemManager.methods.createItem(itemName, cost).send({ from: this.accounts[0] })
+  }
+
   render() {
     if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -64,8 +74,15 @@ class App extends Component {
         <h1>Event Trigger / Supply Chain Example</h1>
         <h2>Items</h2>
         <h2>Add Items</h2>
-        Cost in wei: <input type="text" name="cost" value={this.state.cost} onChange={} />
-        Item identifier: <input type="text" name="cost" value={this.state.cost} onChange={} />
+        <label>
+          Cost in wei:
+        </label>
+        <input type="text" name="cost" value={this.state.cost} onChange={this.handleInputChange} />
+        <label>
+          Item identifier:
+        </label>
+        <input type="text" name="itemName" value={this.state.itemName} onChange={this.handleSubmit} />
+        <button type="button" onClick={this.handleSubmit}>create new item</button>
       </div>
     );
   }
